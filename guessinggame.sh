c721e511b4e02217116a0dd884fb393759c91df7
Bramvd3 <bram.vandendriessche@realimpactanalytics.com>
2020-02-11 18:12:01 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
-echo "Welcome, Try to guess how many lines are in this directory!"
-directory=$(pwd)
-echo "The current directory is $directory."
-read -p "Enter your guess: " guess
-lines=$(ls -l | wc -l)
-let lines=$(expr $lines-1)
-while [[ $lines -ne $guess ]]
-do
-if [[ $guess -gt $lines ]]
-then
-        echo "Your guess was to high, try again."
+function guessinggame() {
+	echo "Welcome, Try to guess how many files are in this directory!"
+	directory=$(pwd)
+	echo "The current directory is $directory."
 	read -p "Enter your guess: " guess
-else
-        echo "Your guess was to low, try again."
-	read -p "Enter your guess: " guess
-fi
-done
-echo "Congratulations, you guessed right!"
-echo "There are indeed $lines lines in this directory."
+	lines=$(ls -lA | grep -v /)
+	let lines=$(expr $lines-1)
+	while [[ $lines -ne $guess ]]
+	do
+	if [[ $guess -gt $lines ]]
+	then
+	        echo "Your guess was to high, try again."
+		read -p "Enter your guess: " guess
+	else
+	        echo "Your guess was to low, try again."
+		read -p "Enter your guess: " guess
+	fi
+	done
+	echo "Congratulations, you guessed right!"
+	echo "There are indeed $lines lines in this directory."
+}
+guessinggame
